6c3bb488bfd77f1c9f12be865d4d10da70cbea2d
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2023-11-24 21:39:37 -0300

    initial: subindo o projeto

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, Text, StyleSheet, Platform, StatusBar } from 'react-native';
+
+import Home from './src/telas/Home';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView style={estilos.AndroidSafeArea}>
+      <Home />
+    </SafeAreaView>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const estilos = StyleSheet.create({
+  AndroidSafeArea: {
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
+  }
+})
